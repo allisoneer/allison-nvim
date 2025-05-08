2b5f1bbe3ff1993252d5a37ff01c57852c32c8dd
Allison Durham <allison@humanlayer.dev>
2025-05-07 21:54:19 -0700

    improve relative path copy to support better

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Store initial working directory for relative path calculations
+vim.g.initial_working_directory = vim.fn.getcwd()
+
 require("allison.settings")
 require("allison.keymap")
 require("allison.lazy")
