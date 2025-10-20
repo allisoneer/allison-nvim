15ca2ab11b03e067a8088ede1ef042e023e52880
Allison Durham <contact@alisoneer.com>
2025-10-20 13:11:14 -0700

    fix: enable Oil to intercept directory arguments
    Add init function to Oil plugin that:
    - Disables netrw early to prevent hijacking directory buffers
    - Registers VimEnter autocmd to detect single directory CLI args
    - Opens Oil for directories while preserving lazy loading
    
    Remove netrw settings from settings.lua since netrw is now fully disabled.
    
    Fixes issue where 'nvim .' would open netrw instead of Oil.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,6 +7,33 @@ return {
 		{ "<leader>e", function() require("oil").open() end, desc = "Open file explorer" },
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	init = function()
+		-- Disable netrw early so it never hijacks directory buffers
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		-- If Neovim was started with exactly one directory argument, open Oil
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				-- count of "file" args (ignores -c, -S etc.)
+				if vim.fn.argc(-1) ~= 1 then
+					return
+				end
+				local arg = vim.fn.argv(0)
+				if not arg or arg == "" then
+					return
+				end
+				local uv = vim.uv or vim.loop
+				local stat = uv.fs_stat(arg)
+				if stat and stat.type == "directory" then
+					-- Schedule to avoid race conditions with startup
+					vim.schedule(function()
+						vim.cmd(string.format("Oil %s", vim.fn.fnameescape(arg)))
+					end)
+				end
+			end,
+		})
+	end,
 	config = function()
 		local oil = require("oil")
 		oil.setup({
