9e6e2ff8d66bf9553b7fb46edaf025767e267b8a
Allison Durham <contact@alisoneer.com>
2025-10-20 11:52:16 -0700

    feat: add git-signs integration with Trouble
    - Add <leader>dg keymap to show git hunks in Trouble
    - Integrates git-signs with Trouble for better git hunk navigation
    - Complements existing <leader>dd for general diagnostics

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,6 +3,7 @@ return {
 	cmd = "Trouble",
 	keys = {
 		{ "<leader>dd", function() require("trouble").toggle() end, desc = "Toggle Trouble" },
+		{ "<leader>dg", function() require("trouble").toggle("git_signs") end, desc = "Git hunks (Trouble)" },
 	},
 	opts = {},
 }
