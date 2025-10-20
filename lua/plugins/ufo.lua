cfe7b199fb05322f3674d5633a2c2c182026e712
Allison Durham <contact@alisoneer.com>
2025-10-20 11:51:24 -0700

    feat: add UFO folding keymaps
    - Add zR to open all folds
    - Add zM to close all folds
    - Add zr to open folds except kinds
    - Add zm to close folds with
    - Improves code folding workflow

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -4,6 +4,12 @@ return {
 	dependencies = {
 		"kevinhwang91/promise-async",
 	},
+	keys = {
+		{ "zR", function() require("ufo").openAllFolds() end, desc = "Open all folds" },
+		{ "zM", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
+		{ "zr", function() require("ufo").openFoldsExceptKinds() end, desc = "Open folds (except kinds)" },
+		{ "zm", function() require("ufo").closeFoldsWith() end, desc = "Close folds with" },
+	},
 	config = function()
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
